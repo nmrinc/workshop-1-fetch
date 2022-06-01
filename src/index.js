89207bfbe2c63691eadf22deb484941ba9e6f534
Erick Avila <eavila@interware.com.mx>
2022-05-31 19:34:38 -0500

    feat(project): first Commit - Class 13
    This is the first commit for the repo. Also belongs to the 13th class that starts the project
    fetching data from API & rendering first elements to body
    
    BREAKING CHANGE: Starts the project with Snowpack, tailwind and added commitizen for sick of mind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const baseUrl = 'https://platzi-avo.vercel.app';
+const { data } = await window
+	.fetch(`${baseUrl}/api/avo`)
+	.then((response) => response.json());
 
-console.log('Happy hacking :)')
+const appNode = document.querySelector('#app');
+
+const items = data.map(({ name: _name, price: _price, image: _image }) => {
+	const container = document.createElement('div');
+	const image = document.createElement('img');
+	image.src = `${baseUrl}${_image}`;
+	const title = document.createElement('h2');
+	title.textContent = _name;
+	const price = document.createElement('p');
+	price.textContent = _price;
+
+	container.append(image, title, price);
+	return container;
+});
+
+appNode.append(...items);
